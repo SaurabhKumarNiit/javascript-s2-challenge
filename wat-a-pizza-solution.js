ef3bd9178285e5c06e2108c3aeba4ef50ef2fdb5
Saurabh Kumar Tiwari <sky.royaltiwari@gmail.com>
2022-10-08 11:38:09 +0530

    completed

--- wat-a-pizza-solution.js
+++ wat-a-pizza-solution.js
@@ -76,19 +76,121 @@ const order = {
 };
 
 //Write function to filter menu items by Category and sort them alphabetically
+var FilterItems;
+function sortByCategory(arr, Category) {
+     FilterItems = arr.filter(items => items.category == Category)
+        .sort((a, b) => {
+            if (a.name < b.name)
+                return -1;
+            else if (a.name > b.name)
+                return 1
+            else
+                return 0;
+        })
+    //console.log(FilterItems);
+    return FilterItems;
+    //let sortItems=FilterItems.sort();
+
+}
+
+sortByCategory(menu, "Beverages");
+sortByCategory(menu, "Starters");
+sortByCategory(menu, "Main Meal");
+
+
 
 //Write function to calculate the amount of each ordered item
+var calculate;
+function calculateAmount(order) {
+
+    //let amountCal=0;
+    calculate = order.map(items => {
+
+        return (items.price) * (items.quantity);
+    })
+
+}
+calculateAmount(order.items);
+//console.log("Calculated Amount Of Each Order : $ "+calculate);
 
 //Write function to calculate the subtotal amount for the main meal ordered
 
+let mainMenuTotal;
+function calculateMainMealAmount() {
+
+    sortByCategory(menu,"Main Meal")
+
+    mainMenuTotal=order.items.filter(items=>{
+        let temp=0;
+        FilterItems.forEach(element=>{
+            if(element.name===items.name){
+                temp=1;
+            }
+        });
+        if(temp==1){
+            return true
+        }
+    }).reduce(function(accumulator,currentValue){
+        return accumulator+(currentValue.price)*(currentValue.quantity);
+
+    },0);
+
+}
+calculateMainMealAmount();
+//console.log("Main Meal Total Amount : "+ mainMenuTotal);
+
 //Write function to find the totalAmount of the order based on the calculated mainMealAmount value
+let sum = 0; 
+var TotalAmount;
+var cokeAmount;
+function calculateTotalAmount(orderAmount, mainMealAmount){
+/*Go through each item in the array and execute this function which adds
+each item to sum 
+*/
+  orderAmount.forEach(item => {
+    sum += item;
+  });
+
+    if(mainMealAmount>40){
+
+        order.items.map(items=>{
+            if(items.name=='Coke'){
+                //cokeAmount=(items.price) * (items.quantity);
+                cokeAmount=items.price;
+                TotalAmount=sum-cokeAmount;
+                return TotalAmount;
+            }
+
+        })
+    
+    }
+    else {
+        console.log(sum);
+        return sum;
+    }       
+}
+
+
 
 //Write function to calculate the final amount after discount
+var FinalAmount;
+function calculatedFinalAmount(totalAmount, discount){
 
-//Write function to display the total amount 
+    if(totalAmount>50){
 
+    FinalAmount=totalAmount-((totalAmount/100)*discount);
+    return FinalAmount;
+}
+else{
+    return totalAmount;
+}
+}
+//Write function to display the total amount
+calculateTotalAmount(calculate,mainMenuTotal);
+console.log("Total Amount payable:                $ "+TotalAmount);
 //Write function to display the final amount
-
+calculatedFinalAmount(TotalAmount,5);
+console.log("Final Amount payable after discount: $ "+FinalAmount);
 //Invoke  functions 
 
 
